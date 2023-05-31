8cf8b6019e1882f9d21c7b5efdc88ee91f8a6eff
MaleeshaSKS <135029430+MaleeshaSKS@users.noreply.github.com>
2023-05-31 10:16:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['19096108799', 'sandaru', true],
+  ['19096108799', '', true], 
   [''] 
 ] //Number of owners
 
@@ -47,7 +47,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'SANDARU-YT'
+global.botname = 'Maleesha - MD'
 global.premium = 'true'
 global.packname = 'GURU┃ᴮᴼᵀ' 
 global.author = '@Asliguru' 
